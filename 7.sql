81f34022952b3596d1d3ae560522a1acc2557d9c
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 09:47:01 -0600

    Print HH:mm:ss for dep_time in 6

--- 7.sql
+++ 7.sql
@@ -1,20 +1,17 @@
 prompt Question 7 - rhanders
 
 CREATE VIEW available_flights AS
-SELECT * FROM
-(
-  SELECT  f.flightno, sf.dep_date,
-          f.src, f.dst, f.dep_time,
-          (f.dep_time+f.est_dur+(b.tzone-a.tzone)) as arr_time,
-          fr.fare,
-          1 as seats,--(fr.limit-(SELECT COUNT(1) FROM b)) as seats,
-          fr.price
-  FROM  flights f, sch_flights sf, flight_fares fr,
-        airports a, airports b, bookings b
-  WHERE f.flightno = sf.flightno AND
-        f.flightno = fr.flightno AND
-        f.flightno = b.flightno AND
-        a.acode = f.src AND
-        b.acode = f.dst
-)
-WHERE seats > 0;
+SELECT  f.flightno, sf.dep_date,
+        f.src, f.dst, f.dep_time,
+        (f.dep_time+f.est_dur+(b.tzone-a.tzone)) as arr_time,
+        fr.fare,
+        1 as seats,--(fr.limit-(SELECT COUNT(1) FROM b)) as seats,
+        fr.price
+FROM  flights f, sch_flights sf, flight_fares fr,
+      airports a, airports b, bookings b
+WHERE f.flightno = sf.flightno AND
+      f.flightno = fr.flightno AND
+      f.flightno = b.flightno AND
+      a.acode = f.src AND
+      b.acode = f.dst
+HAVING seats > 1;
--- README.txt
+++ README.txt
@@ -11,7 +11,7 @@ Query 2: [x] written [ ] tested
 Query 3: [x] written [ ] tested
 Query 4: [x] written [ ] tested
 Query 5: [x] written [ ] tested
-Query 6: [x] written [ ] tested // TODO use actual_dep_time ?
+Query 6: [x] written [ ] tested
 Query 7: [ ] written [ ] tested
 Query 8: [x] written [ ] tested
 Query 9: [ ] written [ ] tested

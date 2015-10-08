aee98dbf83ae95c76187722e2d99a4f8bd04aa98
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 00:50:48 -0600

    Write Query 8

--- 8.sql
+++ 8.sql
@@ -1,1 +1,6 @@
 prompt Question 8 - rhanders
+
+SELECT af.flightno, af.fare, af.seats af.price
+FROM available_flights af
+WHERE to_char(af.dep_date, 'Mon DD, YYYY' = 'Dec 22, 2015'
+ORDER BY af.price DESC;

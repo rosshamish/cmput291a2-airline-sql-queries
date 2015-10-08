71c2307983244b0b46ea29f3638da05c7d87c973
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 00:05:14 -0600

    Write Query 6

--- 6.sql
+++ 6.sql
@@ -1,1 +1,14 @@
 prompt Question 6 - rhanders
+
+SELECT *
+FROM
+(
+  SELECT f.flightno, f.dep_time, fr.fare as ft, fr.price
+  FROM flights f, sch_flights sf, flight_fares fr
+  WHERE f.flightno = sf.flightno AND
+        sf.flightno = fr.flightno AND
+        f.src = 'YEG' AND
+        f.dst = 'LAX'
+  ORDER BY fr.price
+) t
+WHERE ROWNUM = 1;

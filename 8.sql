cae044b4e130b358db3f5c8f9b05e0ba222caa59
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 10:34:41 -0600

    Query 8: bugfixes, add src='YEG', add dst='YYZ'

--- 8.sql
+++ 8.sql
@@ -1,6 +1,8 @@
 prompt Question 8 - rhanders
 
-SELECT af.flightno, af.fare, af.seats af.price
-FROM available_flights af
-WHERE to_char(af.dep_date, 'Mon DD, YYYY' = 'Dec 22, 2015'
-ORDER BY af.price DESC;
+select af.flightno, af.fare, af.seats, af.price
+from available_flights af
+where to_char(af.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015' and
+      af.src = 'YEG' and
+      af.dst = 'YYZ'
+order by af.price asc;

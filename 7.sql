b74232b218ffa287170d9f9ac380bbcd21df07e2
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 00:29:21 -0600

    Query 7: flightno,dep_date,src,dst,dep_time,arr_time

--- 7.sql
+++ 7.sql
@@ -1,1 +1,13 @@
 prompt Question 7 - rhanders
+
+CREATE VIEW available_flights AS
+SELECT  f.flightno, sf.dep_date,
+        f.src, f.dst, f.dep_time,
+        (f.dep_time+f.est_dur+(b.tzone-a.tzone) as arr_time,
+        fare, seats, price
+FROM flights f, sch_flights sf, airports a, airports b
+WHERE f.flightno = sf.flightno AND
+      arr_times.flightno = f.flightno AND
+      a.acode = f.src AND
+      b.acode = f.dst
+

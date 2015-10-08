6b76193648999c00d91c15ca483df209ecb4909b
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 11:12:50 -0600

    Finish Query 10

--- 10.sql
+++ 10.sql
@@ -1,26 +1,29 @@
 prompt Question 10 - rhanders
 
-SELECT * FROM
+SELECT flightno1, flightno2, layover*24 as layover_h, price
+FROM
 (
-  SELECT  gc.flightno1,
-          gc.flightno2,
-          gc.layover,
-          gc.price
-  FROM good_connections gc
-  WHERE gc.src = 'YEG' AND
-        gc.dst = 'LAX' AND
-        to_char(gc.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
+  (
+    SELECT  gc.flightno1,
+            gc.flightno2,
+            gc.layover,
+            gc.price
+    FROM good_connections gc
+    WHERE gc.src = 'YEG' AND
+          gc.dst = 'LAX' AND
+          to_char(gc.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
+  )
+  UNION
+  (
+    SELECT  af.flightno as flightno1,
+            to_char(null) as flightno2,
+            to_number(null) as layover,
+            af.price
+    FROM available_flights af
+    WHERE af.src = 'YEG' AND
+          af.dst = 'LAX' AND
+          to_char(af.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
+  )
 )
-UNION
-(
-  SELECT  af.flightno as flightno1,
-          NULL as flightno2
-          NULL as layover,
-          af.price
-  FROM available_flights af
-  WHERE af.src = 'YEG' AND
-        af.dst = 'LAX' AND
-        to_char(af.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
-)
-ORDER BY price DESC;
-WHERE ROWNUM <= 5;
+WHERE ROWNUM <= 5
+ORDER BY price ASC;

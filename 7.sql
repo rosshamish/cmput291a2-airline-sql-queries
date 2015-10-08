e877ded3b32411a341a87bbb6776ed34b15e3580
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 00:39:13 -0600

    Many duplications in 7, and no seats

--- 7.sql
+++ 7.sql
@@ -1,13 +1,20 @@
 prompt Question 7 - rhanders
 
 CREATE VIEW available_flights AS
-SELECT  f.flightno, sf.dep_date,
-        f.src, f.dst, f.dep_time,
-        (f.dep_time+f.est_dur+(b.tzone-a.tzone) as arr_time,
-        fare, seats, price
-FROM flights f, sch_flights sf, airports a, airports b
-WHERE f.flightno = sf.flightno AND
-      arr_times.flightno = f.flightno AND
-      a.acode = f.src AND
-      b.acode = f.dst
-
+SELECT * FROM
+(
+  SELECT  f.flightno, sf.dep_date,
+          f.src, f.dst, f.dep_time,
+          (f.dep_time+f.est_dur+(b.tzone-a.tzone)) as arr_time,
+          fr.fare,
+          1 as seats,--(fr.limit-(SELECT COUNT(1) FROM b)) as seats,
+          fr.price
+  FROM  flights f, sch_flights sf, flight_fares fr,
+        airports a, airports b, bookings b
+  WHERE f.flightno = sf.flightno AND
+        f.flightno = fr.flightno AND
+        f.flightno = b.flightno AND
+        a.acode = f.src AND
+        b.acode = f.dst
+)
+WHERE seats > 0;

cd912d24f112540084b4dc708bb8bad9843de03e
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-08 10:41:59 -0600

    Query 9 almost working, remove extraneous () from 10

--- 10.sql
+++ 10.sql
@@ -2,27 +2,25 @@ prompt Question 10 - rhanders
 
 SELECT * FROM
 (
-  (
-    SELECT  gc.flightno1,
-            gc.flightno2,
-            gc.layover,
-            gc.price
-    FROM good_connections gc
-    WHERE gc.src = 'YEG' AND
-          gc.dst = 'LAX' AND
-          to_char(gc.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
-  )
-  UNION
-  (
-    SELECT  af.flightno as flightno1,
-            NULL as flightno2
-            NULL as layover,
-            af.price
-    FROM available_flights af
-    WHERE af.src = 'YEG' AND
-          af.dst = 'LAX' AND
-          to_char(af.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
-  )
-  ORDER BY price DESC;
+  SELECT  gc.flightno1,
+          gc.flightno2,
+          gc.layover,
+          gc.price
+  FROM good_connections gc
+  WHERE gc.src = 'YEG' AND
+        gc.dst = 'LAX' AND
+        to_char(gc.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
 )
+UNION
+(
+  SELECT  af.flightno as flightno1,
+          NULL as flightno2
+          NULL as layover,
+          af.price
+  FROM available_flights af
+  WHERE af.src = 'YEG' AND
+        af.dst = 'LAX' AND
+        to_char(af.dep_date, 'Mon DD, YYYY') = 'Dec 22, 2015'
+)
+ORDER BY price DESC;
 WHERE ROWNUM <= 5;
--- 9.sql
+++ 9.sql
@@ -1,9 +1,17 @@
 prompt Question 9 - rhanders
 
-CREATE VIEW good_conections AS
-SELECT src, dst,
-       dep_date,
-       flightno1, flightno2,
-       layover,
-       price
-
+create view good_conections as
+select * from
+(
+  SELECT af1.src, af2.dst,
+         af1.dep_date,
+         af1.flightno as flightno1,
+         af2.flightno as flightno2,
+         to_date(af1.arr_time,'HH24:MI:SS')/(24*60)
+           - to_date(af2.dep_time,'HH24:MI:SS')/(24*60) as layover,
+         af1.price+af2.price as price
+  from available_flights af1, available_flights af2
+  where af1.dst = af2.src
+)
+where layover > 90 and
+      layover < (5*60);
